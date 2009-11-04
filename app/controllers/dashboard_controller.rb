b10bc783e08e5a9da818bd4895ecb916f1761fa1
portfonica <portfonica@gmail.com>
2009-11-04 04:02:43 +0100

    Generated Dashboard controller and index view + basic version of layouts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
 # Author: Martin Hrdlička, 208297@mail.muni.cz
-class ApplicationController < ActionController::Base
+class DashboardController < ApplicationController
+  def index; end
 end
